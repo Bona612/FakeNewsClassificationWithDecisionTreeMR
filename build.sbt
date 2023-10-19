93f7d053d8c585dbe53228f2d3836ab99379e84b
Matteo Boccali <matteo.boccali.06@gmail.com>
2023-10-19 15:44:49 +0200

    Matteo changes

--- build.sbt
+++ build.sbt
@@ -1,12 +1,18 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.13.12"
+ThisBuild / scalaVersion := "2.12.15"
 
-// https://mvnrepository.com/artifact/org.apache.spark/spark-core
-libraryDependencies += "org.apache.spark" %% "spark-core" % "3.5.0"
-
-// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
-libraryDependencies += "org.apache.spark" %% "spark-sql" % "3.5.0" % "provided"
+libraryDependencies ++= Seq(
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-core
+  "org.apache.spark" %% "spark-core" % "3.4.0",
+  // https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+  "org.apache.spark" %% "spark-sql" % "3.4.0" % "provided",
+  "org.apache.spark" %% "spark-mllib" % "3.4.0",
+  // https://mvnrepository.com/artifact/com.johnsnowlabs.nlp/spark-nlp
+  "com.johnsnowlabs.nlp" %% "spark-nlp" % "5.1.0",
+  // https://mvnrepository.com/artifact/org.tartarus/snowball
+  "com.github.rholder" % "snowball-stemmer" % "1.3.0.581.1"
+)
 
 
 lazy val root = (project in file("."))
--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -3,7 +3,6 @@ import org.apache.hadoop.conf.Configuration
 
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
-import org.joda.time.DateTime
 import dataacquisition.DataAcquisition
 import java.nio.file.{Files, Path, Paths}
 import scala.language.postfixOps
@@ -14,6 +13,7 @@ object MainApp {
   final val spark: SparkSession = SparkSession.builder()
                                   .master("local[*]")
                                   .appName("Fake News Classification")
+                                  .config("spark.jars.packages", "com.johnsnowlabs.nlp:spark-nlp_2.12:5.1.0")
                                   .getOrCreate()
 
 
@@ -24,13 +24,27 @@ object MainApp {
     val datasetPath = "./data/dataset"
     val csv = "dataset.csv"
 
+    /*
     // Creating a list of strings
-    val kaggleDatasetList: List[String] = List("therohk/million-headlines", "clmentbisaillon/fake-and-real-news-dataset", "mrisdal/fake-news")
+    val kaggleDatasetList: List[String] = List("hassanamin/textdb3")
     // Create a Map
-    val csvPerDataset: Map[String, String] = Map("million-headlines" -> "abcnews-date-text.csv", "fake-and-real-news-dataset" -> "Fake.csv", "fake-news" -> "fake.csv")
-    val columnsMap: Map[String, String] = Map("million-headlines" -> "headline_text", "fake-and-real-news-dataset" -> "title", "fake-news" -> "title")
+    val csvPerDataset: Map[String, String] = Map("textdb3" -> "fake_or_real_news.csv")
+    val columnsMap: Map[String, String] = Map("textdb3" -> "title")
+    */
+    /*
+    // Creating a list of strings
+    val kaggleDatasetList: List[String] = List("therohk/million-headlines", "clmentbisaillon/fake-and-real-news-dataset", "mrisdal/fake-news", "hassanamin/textdb3", "jruvika/fake-news-detection", "saurabhshahane/fake-news-classification", "stevenpeutz/misinformation-fake-news-text-dataset-79k", "jainpooja/fake-news-detection", "niranjanank/fake-news-log", "mohammadaflahkhan/fake-news-dataset-combined-different-sources")
+    // Create a Map
+    val csvPerDataset: Map[String, String] = Map("million-headlines" -> "abcnews-date-text.csv", "fake-and-real-news-dataset" -> "Fake.csv", "fake-news" -> "fake.csv", "textdb3" -> "fake_or_real_news.csv", "fake-news-detection" -> "data.csv", "fake-news-classification" -> "WELFake_Dataset.csv", "misinformation-fake-news-text-dataset-79k" -> "DataSet_Misinfo_FAKE.csv", "fake-news-detection" -> "Fake.csv", "fake-news-log" -> "train.csv", "fake-news-dataset-combined-different-sources" -> "PreProcessedData.csv")
+    val columnsMap: Map[String, String] = Map("million-headlines" -> "headline_text", "fake-and-real-news-dataset" -> "title", "fake-news" -> "title", "textdb3" -> "title", "fake-news-detection" -> "Headline", "fake-news-classification" -> "title", "misinformation-fake-news-text-dataset-79k" -> "text", "fake-news-detection" -> "title", "fake-news-log" -> "title", "fake-news-dataset-combined-different-sources" -> "title")
     val textColumn: String = "title"
-
+    */
+    // Creating a list of strings
+    val kaggleDatasetList: List[String] = List("mrisdal/fake-news")
+    // Create a Map
+    val csvPerDataset: Map[String, String] = Map("fake-news" -> "fake.csv")
+    val columnsMap: Map[String, String] = Map("fake-news" -> "title")
+    val textColumn: String = "title"
 
     // Use the Files.list method to get a Stream of paths in the directory
     val directoryStream = Files.list(Paths.get(datasetPath))
