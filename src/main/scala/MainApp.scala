1196a91cc5f9875e0d2cfd007a458e4f0ed95a91
Luca Cotugno <cotugno.luca@virgilio.it>
2023-11-04 20:19:52 +0100

    Resolved problem in prediction function

--- src/main/scala/MainApp.scala
+++ src/main/scala/MainApp.scala
@@ -22,14 +22,19 @@ object MainApp {
 
   def main(args: Array[String]): Unit = {
 
-    val decisionTreePath = "/Users/andreamancini/decisiontree"
-    var directoryStream = Files.list(Paths.get(decisionTreePath))
-    var fileList = directoryStream.toArray
+    val decisionTreePath = "/Users/luca/Desktop/tree.txt"
+
+    val tree = DecisionTree.fromFile(decisionTreePath)
+
+    //var directoryStream = Files.list(Paths.get(decisionTreePath))
+    //var fileList = directoryStream.toArray
 
     val currentDir = new File(".").getCanonicalPath
     println(s"currentDir $currentDir")
 
+    /*
     var tree: DecisionTree = null
+
     fileList.foreach { path =>
       if (path.asInstanceOf[Path].toString == Paths.get(decisionTreePath).resolve("tree.txt").toString) {
 
@@ -39,6 +44,7 @@ object MainApp {
 
     if(tree != null)
       return
+     */
     /*
     var node : DecisionTree = Node("Prova",0.3,null,null,None)
     var tmp = node
@@ -79,10 +85,10 @@ object MainApp {
 
 
     // Use the Files.list method to get a Stream of paths in the directory
-    directoryStream = Files.list(Paths.get(datasetPath))
+    //directoryStream = Files.list(Paths.get(datasetPath))
 
     var isDatasetPresent: Boolean = false
-
+/*
     // Convert the Stream to a Scala List and print the file names
     fileList = directoryStream.toArray
     fileList.foreach { path =>
@@ -90,9 +96,9 @@ object MainApp {
         isDatasetPresent = true
       }
     }
-
+  */
     // Close the directory stream
-    directoryStream.close()
+    //directoryStream.close()
 
     var dataset: DataFrame = null
     // If the dataset isn't created, load the dataset and save it
@@ -101,25 +107,99 @@ object MainApp {
       dataset = dataAcquisition.loadDataset()
       println("Dataset loaded succesfully!")
     }
+    print(dataset)
 
     val dataPreparation: MapReduceAlgorithm = new MapReduceAlgorithm(dataset)
     val decTree = dataPreparation.initAlgorithm()
-    decTree.asInstanceOf[Node].writeRulesToFile("/Users/andreamancini/decisiontree/treeOutput.txt")
+    decTree.asInstanceOf[Node].writeRulesToFile("/Users/luca/Desktop/treeOutput.txt")
 
-    /*
-    println("Read dataset!")
-    // Read the dataset
-    val dataset = spark.read.format("csv").option("header", "true").load(Paths.get(datasetPath).resolve(csv).toString)
+    val predictedLabels = decTree.predict(dataset,decTree)
 
-    println("Show dataset")
-    dataset.show()
-    */
+    println("predictedLabels")
+    println(predictedLabels.mkString("Array(", ", ", ")"))
+    val tmp : Array[Any] = dataset.select("label").collect().map(row => row(0))
+    val trueLabels : Array[Int] = tmp.collect{
+      case i : Int => i
+    }
 
+    println("trueLabels")
+    println(trueLabels.mkString("Array(", ", ", ")"))
+
+
+    val (truePositives, falsePositives, falseNegatives) = calculateMetrics(trueLabels, predictedLabels)
+
+    // Print the results
+    println(s"True Positives: $truePositives")
+    println(s"False Positives: $falsePositives")
+    println(s"False Negatives: $falseNegatives")
+
+    // Calculate precision, recall, and F1-score
+    val precision = truePositives.toDouble / (truePositives + falsePositives)
+    val recall = truePositives.toDouble / (truePositives + falseNegatives)
+    val f1Score = 2 * (precision * recall) / (precision + recall)
+
+    // Print precision, recall, and F1-score
+    println(s"Precision: $precision")
+    println(s"Recall: $recall")
+    println(s"F1-Score: $f1Score")
 
     println("Stopping Spark")
     // Stop the Spark session
     spark.stop()
     println("Stop")
   }
+
+  def calculateMetrics(trueLabels: Array[Int], predictedLabels: Array[Int]): (Int, Int, Int) = {
+    if (trueLabels.length != predictedLabels.length) {
+      throw new IllegalArgumentException("Input arrays must have the same length.")
+    }
+
+    var truePositives = 0
+    var falsePositives = 0
+    var falseNegatives = 0
+
+    for ((trueLabel, predictedLabel) <- trueLabels zip predictedLabels) {
+      if (trueLabel == 1 && predictedLabel == 1) {
+        truePositives += 1
+      } else if (trueLabel == 0 && predictedLabel == 1) {
+        falsePositives += 1
+      } else if (trueLabel == 1 && predictedLabel == 0) {
+        falseNegatives += 1
+      }
+    }
+
+    (truePositives, falsePositives, falseNegatives)
+  }
+
+  object MetricsCalculator {
+    def main(args: Array[String]): Unit = {
+      // Example: True labels and predicted labels
+      val trueLabels = Array(1, 0, 1, 0, 1, 0, 0, 1, 1, 0)
+      val predictedLabels = Array(1, 0, 0, 0, 1, 1, 0, 1, 1, 1)
+
+      // Calculate true positives, false positives, and false negatives
+      val (truePositives, falsePositives, falseNegatives) = calculateMetrics(trueLabels, predictedLabels)
+
+      // Print the results
+      println(s"True Positives: $truePositives")
+      println(s"False Positives: $falsePositives")
+      println(s"False Negatives: $falseNegatives")
+
+      // Calculate precision, recall, and F1-score
+      val precision = truePositives.toDouble / (truePositives + falsePositives)
+      val recall = truePositives.toDouble / (truePositives + falseNegatives)
+      val f1Score = 2 * (precision * recall) / (precision + recall)
+
+      // Print precision, recall, and F1-score
+      println(s"Precision: $precision")
+      println(s"Recall: $recall")
+      println(s"F1-Score: $f1Score")
+    }
+
+  }
+
+
+
+
 
 }
--- src/main/scala/decisiontree/DecisionTree.scala
+++ src/main/scala/decisiontree/DecisionTree.scala
@@ -1,6 +1,8 @@
 package decisiontree
 
+import org.apache.spark.sql.functions.{lit, monotonically_increasing_id, row_number}
 import org.apache.spark.sql.{DataFrame, Row}
+import org.apache.spark.sql.expressions.Window
 
 import java.io._
 import scala.io.Source
@@ -10,7 +12,7 @@ sealed trait DecisionTree {
   def printToFile(filename: String, rule: String): String
   def getParent(): Option[DecisionTree]
 
-  def predict(input: DataFrame): Array[Int]
+  def predict(input: DataFrame,decisionTree: DecisionTree): Array[Int]
 }
 
 case class Leaf(label: String,parent: Option[DecisionTree]) extends DecisionTree {
@@ -36,7 +38,7 @@ case class Leaf(label: String,parent: Option[DecisionTree]) extends DecisionTree
 
   override def getParent(): Option[DecisionTree] = {parent}
 
-  override def predict(input: DataFrame): Array[Int] = { return null}
+  override def predict(input: DataFrame,decisionTree: DecisionTree): Array[Int] = { return null}
 }
 
 case class Node(var attribute: String, value: Double, var left: DecisionTree, var right: DecisionTree, parent: Option[Node]) extends DecisionTree {
@@ -51,6 +53,7 @@ case class Node(var attribute: String, value: Double, var left: DecisionTree, va
       writer.close()
     }
   }
+
   def getAttribute(): String = {
     this.attribute
   }
@@ -64,9 +67,9 @@ case class Node(var attribute: String, value: Double, var left: DecisionTree, va
   }
 
   def getLeftChild(): Option[DecisionTree] = {
-    var tmp : Option[DecisionTree] = None
-    if(this.left != null){
-     tmp = Option(this.left)
+    var tmp: Option[DecisionTree] = None
+    if (this.left != null) {
+      tmp = Option(this.left)
     }
     tmp
   }
@@ -95,8 +98,8 @@ case class Node(var attribute: String, value: Double, var left: DecisionTree, va
 
     addedRule = addedRule + attribute.toString
     println("RULE: " + addedRule)
-    val leftRule : String = left.printToFile(filename, addedRule + " < " + value.toString)
-    val rightRule : String = right.printToFile(filename, addedRule + " >= " + value.toString)
+    val leftRule: String = left.printToFile(filename, addedRule + " < " + value.toString)
+    val rightRule: String = right.printToFile(filename, addedRule + " >= " + value.toString)
     val ret: String = leftRule + rightRule
     ret
   }
@@ -107,174 +110,216 @@ case class Node(var attribute: String, value: Double, var left: DecisionTree, va
     println("INIZIO")
     rule = rule + attribute.toString
     println("RULE: " + rule)
-    val leftRule : String = left.printToFile(filename, rule + " < " + value.toString)
-    val rightRule : String = right.printToFile(filename, rule + " >= " + value.toString)
+    val leftRule: String = left.printToFile(filename, rule + " < " + value.toString)
+    val rightRule: String = right.printToFile(filename, rule + " >= " + value.toString)
     val ret: String = leftRule + rightRule
     ret
   }
 
-  override def getParent(): Option[DecisionTree] = { parent }
+  override def getParent(): Option[DecisionTree] = {
+    parent
+  }
 
-  override def predict(input: DataFrame): Array[Int] = {
-    var currentNode = this
-    val res : Array[Int] = new Array[Int](input.count().toInt)
-    print(res.length)
-    input.foreach { row: Row =>
-      while(currentNode.left!=null && currentNode!=null){
-        if (currentNode.getValue() > row.getAs[Double](currentNode.getAttribute()) ){
-          currentNode = currentNode.getLeftChild().asInstanceOf[Node]
-        }
-        else {
-          currentNode = currentNode.getRightChild().asInstanceOf[Node]
+  override def predict(input: DataFrame, decisionTree: DecisionTree): Array[Int] = {
+    var currentNode = Option(decisionTree)
+    // Definisci una finestra per l'ordinamento
+    val windowSpec = Window.orderBy(lit(1))
+
+    // Aggiungi una colonna con l'indice a partire da 0
+    val dfWithIndex = input.withColumn("index", row_number().over(windowSpec) - 1)
+    println("dfWithIndex.show()")
+    dfWithIndex.show()
+    val res: Array[Int] = new Array[Int](dfWithIndex.count().toInt)
+    println("DENTRO PREDICT")
+
+    dfWithIndex.collect().foreach{ row: Row =>
+      var stop: Boolean = true
+
+      while(stop) {
+        currentNode match {
+          case None => {}
+          case Some(value) => {
+            if (value.isInstanceOf[Leaf]) {
+              val index = row.getAs[Int]("index")
+              res(index) = value.asInstanceOf[Leaf].getLabel().toInt
+              println(res(index))
+              stop = false
+              println("RES")
+              println(res.mkString("Array(", ", ", ")"))
+            }
+            else {
+              if (value.asInstanceOf[Node].getValue() > row.getAs[Double](value.asInstanceOf[Node].getAttribute())) {
+                value.asInstanceOf[Node].getLeftChild() match {
+                  case None => {}
+                  case Some(value) => {
+                    if (value.isInstanceOf[Leaf]) {
+                      currentNode = Option(value.asInstanceOf[Leaf])
+                    }
+                    else currentNode = Option(value.asInstanceOf[Node])
+                  }
+                }
+              }
+              else {
+                value.asInstanceOf[Node].getRightChild() match {
+                  case None => {}
+                  case Some(value) => {
+                    if (value.isInstanceOf[Leaf]) {
+                      currentNode = Option(value.asInstanceOf[Leaf])
+                    }
+                    else currentNode = Option(value.asInstanceOf[Node])
+                  }
+                }
+              }
+            }
+          }
         }
       }
-      res(input.collect().indexOf(row)) = currentNode.asInstanceOf[Leaf].getLabel().toInt
     }
-  res
+    println(res.mkString("Array(", ", ", ")"))
+    res
   }
 }
 
+  object DecisionTree {
+    def fromFile(filename: String): DecisionTree = {
+      // Open the file for reading
+      val file = Source.fromFile(filename)
 
-object DecisionTree{
-  def fromFile(filename: String): DecisionTree = {
-    // Open the file for reading
-    val file = Source.fromFile(filename)
+      var nodes: Array[String] = null
+      var values: Array[String] = null
+      var tree: Node = null
+      var current: Option[DecisionTree] = null
+      var attribute: String = null
+      var value: Double = 0.0
+      var operation: String = null
+      var last: String = null
+      var parent: Option[DecisionTree] = null
+      var current_operation: String = null
 
-    var nodes: Array[String] = null
-    var values: Array[String] = null
-    var tree: Node = null
-    var current: Option[DecisionTree] = null
-    var attribute: String = null
-    var value: Double = 0.0
-    var operation: String = null
-    var last: String = null
-    var parent : Option[DecisionTree] = null
-    var current_operation : String = null
+      try {
+        // Iterate over the lines in the file
+        for (line <- file.getLines()) {
+          println(line) // Process the line (in this case, printing it)
+          nodes = line.split(",")
 
-    try {
-      // Iterate over the lines in the file
-      for (line <- file.getLines()) {
-        println(line) // Process the line (in this case, printing it)
-        nodes = line.split(",")
+          // Remove the last element and get it
+          last = nodes.last.trim()
+          var middleNodes = nodes.dropRight(1)
 
-        // Remove the last element and get it
-        last = nodes.last.trim()
-        var middleNodes = nodes.dropRight(1)
+          for (node <- middleNodes) {
+            values = node.trim().split(" ")
+            attribute = values(0)
+            current_operation = values(1)
+            value = values(2).toDouble
 
-        for (node <- middleNodes) {
-          values = node.trim().split(" ")
-          attribute = values(0)
-          current_operation = values(1)
-          value = values(2).toDouble
-
-          if (tree == null) {
-            println("CURRENT == NULL")
-            operation = current_operation
-            tree = Node(attribute, value, null, null, None)
-            current = Option(tree)
-          }
-          else if (current.isEmpty) {
-            println("INSERISCO MANCANTE ")
-            println(attribute + " " + value.toString)
-            if (operation == "<") {
-              parent.get.asInstanceOf[Node].insertLeftChild(Node(attribute, value, null, null, Option(parent.get.asInstanceOf[Node])))
-              current = parent.get.asInstanceOf[Node].getLeftChild()
-              println("INSERISCO MANCANTE LEFT ")
-              println(parent.get.asInstanceOf[Node].getAttribute() + " " + parent.get.asInstanceOf[Node].getValue())
-              println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue())
+            if (tree == null) {
+              println("CURRENT == NULL")
+              operation = current_operation
+              tree = Node(attribute, value, null, null, None)
+              current = Option(tree)
             }
-            else {
-              parent.get.asInstanceOf[Node].insertRightChild(Node(attribute, value, null, null, Option(parent.get.asInstanceOf[Node])))
-              current = parent.get.asInstanceOf[Node].getRightChild()
-              println("INSERISCO MANCANTE RIGHT ")
-              println(parent.get.asInstanceOf[Node].getAttribute() + " " + parent.get.asInstanceOf[Node].getValue())
-              println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue())
-            }
-           /* if(operation == "<"){
+            else if (current.isEmpty) {
+              println("INSERISCO MANCANTE ")
+              println(attribute + " " + value.toString)
+              if (operation == "<") {
+                parent.get.asInstanceOf[Node].insertLeftChild(Node(attribute, value, null, null, Option(parent.get.asInstanceOf[Node])))
+                current = parent.get.asInstanceOf[Node].getLeftChild()
+                println("INSERISCO MANCANTE LEFT ")
+                println(parent.get.asInstanceOf[Node].getAttribute() + " " + parent.get.asInstanceOf[Node].getValue())
+                println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue())
+              }
+              else {
+                parent.get.asInstanceOf[Node].insertRightChild(Node(attribute, value, null, null, Option(parent.get.asInstanceOf[Node])))
+                current = parent.get.asInstanceOf[Node].getRightChild()
+                println("INSERISCO MANCANTE RIGHT ")
+                println(parent.get.asInstanceOf[Node].getAttribute() + " " + parent.get.asInstanceOf[Node].getValue())
+                println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue())
+              }
+              /* if(operation == "<"){
               current = current.get.asInstanceOf[Node].getLeftChild()
             }
             else{
               current = current.get.asInstanceOf[Node].getRightChild()
             }
             */
-            parent = current.get.asInstanceOf[Node].getParent()
-          }
-          else {
-            // FORSE SERVE UN CONTROLLO SUL FATTO CHE POTREBBE ESSERE UNA FOGLIA
-            // MA NELLA REALTà LA FOGLIA LA CAVO PRIMA
-            val current_node: Node = current.get.asInstanceOf[Node]
-            parent = current_node.parent
+              parent = current.get.asInstanceOf[Node].getParent()
+            }
+            else {
+              // FORSE SERVE UN CONTROLLO SUL FATTO CHE POTREBBE ESSERE UNA FOGLIA
+              // MA NELLA REALTà LA FOGLIA LA CAVO PRIMA
+              val current_node: Node = current.get.asInstanceOf[Node]
+              parent = current_node.parent
 
-            if (current_node.getAttribute() != attribute || current_node.getValue() != value) {
-              println("DIVERSO")
-              if (operation == "<") {
-                current_node.insertLeftChild(Node(attribute, value, null, null, Option(current_node)))
-                current = current.get.asInstanceOf[Node].getLeftChild()
-                println("INSERISCO SINISTRA " + current_node.getAttribute() + " " + current_node.getValue().toString)
-                println(attribute + " " + value.toString)
+              if (current_node.getAttribute() != attribute || current_node.getValue() != value) {
+                println("DIVERSO")
+                if (operation == "<") {
+                  current_node.insertLeftChild(Node(attribute, value, null, null, Option(current_node)))
+                  current = current.get.asInstanceOf[Node].getLeftChild()
+                  println("INSERISCO SINISTRA " + current_node.getAttribute() + " " + current_node.getValue().toString)
+                  println(attribute + " " + value.toString)
+                }
+                else {
+                  current_node.insertRightChild(Node(attribute, value, null, null, Option(current_node)))
+                  current = current.get.asInstanceOf[Node].getRightChild()
+                  println("INSERISCO DESTRA " + current_node.getAttribute() + " " + current_node.getValue().toString)
+                  println(attribute + " " + value.toString)
+                }
               }
-              else {
-                current_node.insertRightChild(Node(attribute, value, null, null, Option(current_node)))
-                current = current.get.asInstanceOf[Node].getRightChild()
-                println("INSERISCO DESTRA " + current_node.getAttribute() + " " + current_node.getValue().toString)
-                println(attribute + " " + value.toString)
+              else if (current_node.getAttribute() == attribute && current_node.getValue() == value) {
+                println("UGUALE")
+                if (current_operation == "<") {
+                  println("SCORRIMENTO SINISTRA " + current_node.getAttribute() + " " + current_node.getValue().toString)
+                  println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue().toString)
+                  current = current.get.asInstanceOf[Node].getLeftChild()
+                }
+                else {
+                  println("SCORRIMENTO DESTRA" + current_node.getAttribute() + " " + current_node.getValue().toString)
+                  println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue().toString)
+                  current = current.get.asInstanceOf[Node].getRightChild()
+                }
+                parent = Option(current_node) //current.get.asInstanceOf[Node].getParent()
+                println(parent.isEmpty)
               }
             }
-            else if (current_node.getAttribute() == attribute && current_node.getValue() == value) {
-              println("UGUALE")
-              if (current_operation == "<") {
-                println("SCORRIMENTO SINISTRA " + current_node.getAttribute() + " " + current_node.getValue().toString)
-                println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue().toString)
-                current = current.get.asInstanceOf[Node].getLeftChild()
-              }
-              else {
-                println("SCORRIMENTO DESTRA" + current_node.getAttribute() + " " + current_node.getValue().toString)
-                println(current.get.asInstanceOf[Node].getAttribute() + " " + current.get.asInstanceOf[Node].getValue().toString)
-                current = current.get.asInstanceOf[Node].getRightChild()
-              }
-              parent = Option(current_node) //current.get.asInstanceOf[Node].getParent()
-              println(parent.isEmpty)
+            operation = current_operation
+          }
+
+
+          if (current.isEmpty) {
+            val leaf: Leaf = Leaf(last, parent)
+            println("INSERISCO FOGLIA MANCANTE ")
+            println(attribute + " " + value.toString)
+            if (current_operation == "<") {
+              parent.get.asInstanceOf[Node].insertLeftChild(leaf)
+              println("INSERISCO FOGLIA MANCANTE LEFT ")
+              println(leaf.getLabel())
+            }
+            else {
+              parent.get.asInstanceOf[Node].insertRightChild(leaf)
+              println("INSERISCO FOGLIA MANCANTE RIGHT ")
+              println(leaf.getLabel())
             }
           }
-          operation = current_operation
-        }
-
-
-        if (current.isEmpty) {
-          val leaf: Leaf = Leaf(last,parent)
-          println("INSERISCO FOGLIA MANCANTE ")
-          println(attribute + " " + value.toString)
-          if (current_operation == "<") {
-            parent.get.asInstanceOf[Node].insertLeftChild(leaf)
-            println("INSERISCO FOGLIA MANCANTE LEFT ")
-            println(leaf.getLabel())
-          }
           else {
-            parent.get.asInstanceOf[Node].insertRightChild(leaf)
-            println("INSERISCO FOGLIA MANCANTE RIGHT ")
-            println(leaf.getLabel())
+            val leaf: Leaf = Leaf(last, current)
+            if (current_operation == "<") {
+              current.get.asInstanceOf[Node].insertLeftChild(leaf)
+              println("LABEL LEFT")
+              println(leaf.getLabel())
+            }
+            else {
+              current.get.asInstanceOf[Node].insertRightChild(leaf)
+              println("LABEL RIGHT")
+              println(leaf.getLabel())
+            }
           }
+          current = Option(tree)
+          parent = current.get.asInstanceOf[Node].parent
         }
-        else {
-          val leaf: Leaf = Leaf(last,current)
-          if (current_operation == "<") {
-            current.get.asInstanceOf[Node].insertLeftChild(leaf)
-            println("LABEL LEFT")
-            println(leaf.getLabel())
-          }
-          else {
-            current.get.asInstanceOf[Node].insertRightChild(leaf)
-            println("LABEL RIGHT")
-            println(leaf.getLabel())
-          }
-        }
-        current = Option(tree)
-        parent = current.get.asInstanceOf[Node].parent
+        tree
       }
-      tree
-    }
-    finally {
-      file.close() // Close the file when done
+      finally {
+        file.close() // Close the file when done
+      }
     }
   }
-}
